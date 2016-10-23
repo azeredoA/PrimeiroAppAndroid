e0930bdcac43c441bc47329d14295fc16a7945dc
imnotvirus <OIluiz00>
2016-10-23 17:54:51 -0200

    criados alguns botoes

--- app/src/main/java/com/example/imnotvirus/meuprimeiroapp/ContaPassosActivity.java
+++ app/src/main/java/com/example/imnotvirus/meuprimeiroapp/ContaPassosActivity.java
@@ -9,28 +9,39 @@ import android.widget.TextView;
 public class ContaPassosActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    private Button btnZerar;
+    private Button btnAumentar, btnDiminuir, btnZerar;
     private TextView nContador;
     private int cCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_outra_tela);
+        setContentView(R.layout.activity_conta_passos);
 
-        btnZerar = (Button) findViewById(R.id.ContaPassos_btn_zerar);
-        nContador = (TextView) findViewById(R.id.ContaPassos_txt_contador);
+        btnAumentar = (Button)      findViewById(R.id.ContaPassos_btn_aumentar);
+        btnDiminuir = (Button)      findViewById(R.id.ContaPassos_btn_diminuir);
+        btnZerar    = (Button)      findViewById(R.id.ContaPassos_btn_zerar);
 
+        nContador   = (TextView)    findViewById(R.id.ContaPassos_txt_contador);
+
+        btnAumentar.setOnClickListener(this);
+        btnDiminuir.setOnClickListener(this);
         btnZerar.setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View v) {
-        if (v == btnZerar) {
+        if (v == btnAumentar) {
             cCount++;
             nContador.setText(""+cCount);
-
+        }
+        if (v == btnDiminuir){
+            cCount--;
+            nContador.setText(""+cCount);
+        }
+        if (v == btnZerar){
+            cCount = 0;
+            nContador.setText(""+cCount);
         }
     }
 }
